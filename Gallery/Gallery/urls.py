388efd8377390e6a0403d09aedcbea365e23ed71
alexmwaura <alexmwaura43@gmail.com>
2019-11-11 13:19:12 +0300

    change project url

--- Gallery/Gallery/urls.py
+++ Gallery/Gallery/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import path,include
+from django.conf.urls import url,include
 from django.contrib import admin
 
 
 
 
 urlpatterns = [
-    path(r'admin/', admin.site.urls),
-    path(r'',include('art.urls'))
+    url(r'admin/', admin.site.urls),
+    url(r'',include('art.urls'))
 ]
