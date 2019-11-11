aa6b4a61c7b3a3ffd78f1e12a7dc08713f246a2e
alexmwaura <alexmwaura43@gmail.com>
2019-11-11 14:46:12 +0300

    Add tests for Art class

--- Gallery/art/tests.py
+++ Gallery/art/tests.py
@@ -24,6 +24,29 @@ class CategoryTestCase(TestCase):
         self.category = Category.delete_category(self)
 
 
-        
-        
+class ArtTestCase(TestCase):
+
+    def setUp(self):
+        self.art = Art(art_name ='name',art_description = 'art description')
+        self.art.save_art()
+
+        self.new_location = Location(place = 'testing')
+        self.new_tag.save()
+
+        self.new_art = Art(art_name = 'Test Article',art_description = 'This is a test',category = self.Category)
+        self.new_art.save()
+
+        self.new_art.art_location.add(self.new_art)
+
+
+
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Art.objects.all().delete()
+
+
+ 
+                                        
             
