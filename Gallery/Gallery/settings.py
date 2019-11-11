b2ee6c524c859fe8c504a7a132f75156e0a5c219
alexmwaura <alexmwaura43@gmail.com>
2019-11-11 15:49:29 +0300

    deployment to heroku

--- Gallery/Gallery/settings.py
+++ Gallery/Gallery/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for Gallery project.
 
@@ -122,12 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFIELDS_DIRS = [
-    os.path.join(BASE_DIR,'static_root')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
     
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+django_heroku.settings(locals()) 
